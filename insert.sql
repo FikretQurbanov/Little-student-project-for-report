6842c35bbc9f9b7c786d4b1071229423bdee0ced
Fikret Qurbanov <fikretqurbanov54@gmail.com>
2025-12-18 17:13:25 +0400

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO students (id, name, age, department) VALUES
+(1, 'Ali', 20, 'CS'),
+(2, 'Rafiq', 23, 'DD'),
+(3, 'Kamran', 18, 'FT'),
+(4, 'Namiq', 20, 'CS'),
+(5, 'Qurban', 17, 'DD'),
+(6, 'Qafur', 20, 'CS'),
+(7, 'Namiq', 22, 'FT');
+
+
+INSERT INTO courses (courses_name, student_id) VALUES
+('Database Systems', 1),
+('Operating Systems', 2),
+('Computer Networks', 3),
+('Database Systems', 4),
+('Operating Systems', 5),
+('Computer Networks', 6),
+('Database Systems', 7);
